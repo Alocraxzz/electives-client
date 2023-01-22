ec2e9885e2583a83090125e87aa559676ad9018e
SinedAnoh <alocraxzz@gmail.com>
2023-01-22 23:56:15 +0200

    add routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+import {Electives} from "./pages/Electives";
+import {Exams} from "./pages/Exams";
+import {Students} from "./pages/Students";
+import {Subjects} from "./pages/Subjects";
+
+const router = createBrowserRouter([
+    { path: "/", element: <App /> },
+    { path: "/electives", element: <Electives /> },
+    { path: "/exams", element: <Exams /> },
+    { path: "/students", element: <Students /> },
+    { path: "/subjects", element: <Subjects /> },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
