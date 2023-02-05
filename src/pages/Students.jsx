6233fd2a82d715deafc2b6ea4081ed0fcd813ba4
SinedAnoh <alocraxzz@gmail.com>
2023-02-05 11:27:58 +0200

    update students page

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,30 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import classes from './styles/PagesStyle.module.css';
-import { ResponsiveDrawer } from '../components/Drawer/ResponsiveDrawer';
+import React, { useEffect } from "react";
+import classes from "./styles/PagesStyle.module.css";
+import { ResponsiveDrawer } from "../components/Drawer/ResponsiveDrawer";
 import { useDispatch, useSelector } from "react-redux";
-import { StudentsDataGrid } from '../components/DataGrid/StudentsDataGrid/StudentsDataGrid';
-import StudentService from '../API/StudentService';
 import { fetchStudents } from "../features/redux/rtk/studentSlice";
+import { StudentsDataGrid } from "../components/DataGrid/StudentsDataGrid/StudentsDataGrid";
 
 export const Students = () => {
-    const students = useSelector(state => state.students.students);
-    const dispatch = useDispatch();
-
-
+    const { students, status } = useSelector(state => state.students);
+    const dispatch             = useDispatch();
 
     useEffect(() => {
         dispatch(fetchStudents());
-    }, []);
-
-    useEffect(() => {
-        console.log(students);
     }, [students]);
 
     return (
         <div className={classes.mainContent}>
             <ResponsiveDrawer>
-                {/*<StudentsDataGrid/>*/}
-                {students?.map((student) => <div key={student._id}>{student.firstName}</div>)}
+                <StudentsDataGrid/>
             </ResponsiveDrawer>
         </div>
     );
