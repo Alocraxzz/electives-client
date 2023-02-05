0463df4f5acaf1dd04150b7f4bbb380fc453dfb6
SinedAnoh <alocraxzz@gmail.com>
2023-02-05 11:27:19 +0200

    delete useless code from form dialog

--- src/components/Dialog/FormDialog/FormDialog.jsx
+++ src/components/Dialog/FormDialog/FormDialog.jsx
@@ -1,34 +1,33 @@
-import * as React from 'react'
-import { useEffect, useState } from 'react'
-import Button from '@mui/material/Button'
-import Dialog from '@mui/material/Dialog'
-import DialogActions from '@mui/material/DialogActions'
-import DialogContent from '@mui/material/DialogContent'
-import DialogContentText from '@mui/material/DialogContentText'
-import DialogTitle from '@mui/material/DialogTitle'
+import * as React from "react";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
-export const FormDialog = ({ children, startIcon, openButtonTitle, title, header, maxWidth }) => {
-    const [open, setOpen]           = useState(false)
-    const [fullWidth, setFullWidth] = useState(true)
+export const FormDialog = ({ children, startIcon, openButtonTitle, title, header, maxWidth, handleFormSubmit }) => {
+    const [open, setOpen]           = useState(false);
+    const [fullWidth, setFullWidth] = useState(true);
 
-    const [email, setEmail]         = useState('')
-
-    useEffect(() => {
-        console.log('email: ' + email)
-    }, [email])
+    const handleSubmit = async () => {
+        setOpen(false);
+        handleFormSubmit();
+    };
 
     const handleClickOpen = () => {
-        setOpen(true)
-    }
+        setOpen(true);
+    };
 
     const handleClose = () => {
-        setOpen(false)
-    }
+        setOpen(false);
+    };
 
     return (
         <div>
             <Button variant="outlined" startIcon={startIcon} onClick={handleClickOpen}>
-                {openButtonTitle ?? 'Open dialog'}
+                {openButtonTitle ?? "Open dialog"}
             </Button>
             <Dialog
                 fullWidth={fullWidth}
@@ -45,9 +44,9 @@ export const FormDialog = ({ children, startIcon, openButtonTitle, title, header
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Submit</Button>
+                    <Button onClick={handleSubmit}>Submit</Button>
                 </DialogActions>
             </Dialog>
         </div>
-    )
-}
+    );
+};
