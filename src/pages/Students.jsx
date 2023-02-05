513f2338fe2cf868ebea512aca504375eff73023
SinedAnoh <alocraxzz@gmail.com>
2023-02-05 19:19:27 +0200

    delete useless imports

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import classes from "./styles/PagesStyle.module.css";
 import { ResponsiveDrawer } from "../components/Drawer/ResponsiveDrawer";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchStudents } from "../features/redux/rtk/studentSlice";
+import { useSelector } from "react-redux";
 import { StudentsDataGrid } from "../components/DataGrid/StudentsDataGrid/StudentsDataGrid";
+import { Status } from "../features/redux/rtk/studentSlice";
 
 export const Students = () => {
-
     return (
         <div className={classes.mainContent}>
             <ResponsiveDrawer>
-                <StudentsDataGrid />
+                <StudentsDataGrid/>
             </ResponsiveDrawer>
         </div>
     );
