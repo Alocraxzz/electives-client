5f2116f4bda6b273c3684090188b0a4d4cb739a2
SinedAnoh <alocraxzz@gmail.com>
2023-02-07 09:43:40 +0200

    complete with the table

---
+++ src/components/DataGrid/ExamsDataGrid/StudentsDataGrid.jsx
@@ -0,0 +1,52 @@
+import { DataGridTemplate } from "../DataGrid";
+import { StudentDialog } from "../../Dialog/StudentDialog/StudentDialog";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteOneStudent, fetchStudents, Status } from "../../../features/redux/rtk/studentSlice";
+import { useEffect } from "react";
+import { CircularProgress } from "@mui/material";
+
+const headers = [
+    { type: "string", field: "_id", label: "ID" },
+    { type: "string", field: "firstName", label: "First name" },
+    { type: "string", field: "secondName", label: "Second name" },
+    { type: "string", field: "thirdName", label: "Third name" },
+    { type: "string", field: "phone", label: "Phone" },
+    { type: "string", field: "address", label: "Address" },
+];
+
+export const StudentsDataGrid = () => {
+    const { students, status, isUpdateRequired } = useSelector(state => state.students);
+    const dispatch                               = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchStudents());
+    }, [isUpdateRequired]);
+
+    useEffect(() => {
+        console.log(students);
+    }, [students]);
+
+    const deleteStudent = (id) => {
+        dispatch(deleteOneStudent(id));
+    };
+
+    return (
+        <>
+            <DataGridTemplate
+                data={students}
+                deleteRecord={deleteStudent}
+                formDialog={
+                    <StudentDialog
+                        initialState={
+                            { firstName: "", secondName: "", thirdName: "", phone: "", address: "" }
+                        }
+                    />
+                }
+                headers={headers}
+            />
+            {status === Status.pending &&
+                <CircularProgress color="inherit"/>
+            }
+        </>
+    );
+};
